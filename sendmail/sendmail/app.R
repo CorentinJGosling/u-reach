0eb01150c3969dd9d18b0d08624e632481163703
CorentinJGosling <you@example.com>
2023-12-03 21:11:45 +0100

    03 12 23

--- sendmail/sendmail/app.R
+++ sendmail/sendmail/app.R
@@ -1,5 +1,8 @@
 library(shiny)
 library(shinyforms)
+library(DT)
+library(shinyWidgets)
+library(shinyjs)
 
 # Define the first form: basic information
 basicInfoForm <- list(
@@ -24,11 +27,35 @@ basicInfoForm <- list(
 )
 
 ui <- fluidPage(
-  h1("U-REACH checklist"),
+  tags$head(tags$style(HTML('
+  .show{display:flex;}
+  .hide{display: none;}
+  '))),
+  shinyjs::useShinyjs(),
+  div(id="top",
+      h1("U-REACH checklist"),
+      materialSwitch(
+        inputId = "btnLogin",
+        label = "Admin login",
+        status = "primary",
+        right = TRUE),
+      ),
+  div(id="admin",
+      textInput("user", "Username"),
+      textInput("pwd", "Password"),
+      actionButton("log", "Login")
+      ),
   formUI(basicInfoForm)
 )
 
 server <- function(input, output, session) {
+  observe({
+    enable_login <- isTRUE(input$btnLogin)
+    toggleClass(selector = "#admin", class="hide",
+                condition = !enable_login)
+
+  })
+
   formServer(basicInfoForm)
 }
 
